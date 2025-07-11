169951e2df482b07ffdbbd7f3ffbd837c3a7dc6f
skiifox <adrienlacr@gmail.com>
2025-07-11 16:18:26 +0200

    added services
    jjwdw

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./src/**/*.{astro,html,js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: "#4F46E5", // Indigo
+      },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+};
